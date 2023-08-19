57048e592e1381ccce88c51df4e537ab05d290e4
Ranatorofder <Bayezidalrabbirana@gmail.com>
2023-08-19 20:34:01 +0600

    js

---
+++ js/style-dom.js
@@ -0,0 +1,14 @@
+const sections = document.querySelectorAll('section');
+for (const section of sections) {
+    section.style.border = '5px solid red'
+    section.style.borderRadius = '9px'
+    section.style.marginTop = '5px'
+    section.style.width = '300px'
+    section.style.backgroundColor = 'lightgray'
+
+
+}
+
+const PlacesContainers = document.getElementById('Places-containers');
+
+PlacesContainers.style.backgroundColor = 'yellow'
